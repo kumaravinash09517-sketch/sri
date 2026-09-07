c9dea64250ad772323aa0e54c84a76d381a3a2cf
kumaravinash09517-sketch <kumaravinash09517@gmail.com>
2026-09-07 13:56:34 +0530

    Add root Gradle files and app build.gradle to enable CI build

---
+++ gradlew.bat
@@ -0,0 +1,8 @@
+@echo off
+rem -----------------------------------------------------------------------
+rem Gradle start-up script for Windows
+rem -----------------------------------------------------------------------
+setlocal
+nREM Resolve links - this %~dp0 may be a link
+set DIRNAME=%~dp0nset EXECUTABLE=%DIRNAME%gradle
+"%EXECUTABLE%" %*
